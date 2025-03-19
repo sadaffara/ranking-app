f7f0be01ed254370bcf4a109feaee07c60f775c1
Sadaf Farajian Sharghi <safs@boozt.com>
2025-03-19 18:09:14 +0100

    clean up bulk create function and remove extra functionality

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -38,36 +38,18 @@ export const processSheetData = (
     let updatedUsers = [...users];
     let updatedScores = [...scores];
 
-    // Process each row of the uploaded sheet data
     data.forEach(({ name, score }) => {
         const existingUser = updatedUsers.find(user => user.name.toLowerCase() === name.toLowerCase());
 
         if (existingUser) {
-            // If user exists, add the score to their scores
             updatedScores.push({
                 userId: existingUser._id,
                 score
             });
-
-            // Optionally, update the highest score if needed
-            const currentHighestScore = updatedScores.filter(score => score.userId === existingUser._id)
-                .reduce((max, score) => Math.max(max, score.score), 0);
-
-            // If the new score is higher than the current highest, update the user's highest score
-            if (currentHighestScore > existingUser.highestScore) {
-                const updatedUser = {
-                    ...existingUser,
-                    highestScore: currentHighestScore,
-                };
-
-                // Update the users list with the updated user
-                updatedUsers = updatedUsers.map(user =>
-                    user._id === updatedUser._id ? updatedUser : user
-                );
-            }
         } else {
-            // If user doesn't exist, create a new user and add the score
-            const newUserId = Math.max(...updatedUsers.map(u => u._id)) + 1;
+            const newUserId = updatedUsers.length
+                ? Math.max(...updatedUsers.map(u => u._id)) + 1
+                : 1; // Handle the case where there are no users yet
             const newUser: User = { _id: newUserId, name };
 
             updatedUsers.push(newUser);
