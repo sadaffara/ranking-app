9c12ba416ecca3250e9305a3806212d1202fdd59
Sadaf Farajian Sharghi <safs@boozt.com>
2025-03-19 17:19:03 +0100

    update scores list when accordion is expanded

--- src/components/userScores.tsx
+++ src/components/userScores.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Score, UserScoresProps } from '../types/types';
-import { AccordionPanel } from '@chakra-ui/react';
+import { AccordionPanel, Divider } from '@chakra-ui/react';
 import { getUserScoresList } from '../utils/helpers';
+import { H4 } from '@northlight/ui';
 
 const UserScores: React.FC<UserScoresProps> = ({
     isExpanded,
@@ -16,15 +17,19 @@ const UserScores: React.FC<UserScoresProps> = ({
             const userScores = getUserScoresList(userId, scores);
             setUserScores(userScores)
         }
-    }, [isExpanded]);
+    }, [isExpanded, scores]);
 
     return (
         <AccordionPanel pb={12}>
+            <div>
+            <Divider />
+            <H4 marginBottom="2" marginTop="2">Score List:</H4>
             {userScores.map((item) => (
                 <div key={item.userId}>
-                    <p>Score: {item.score}</p>
+                    <p>{item.score}</p>
                 </div>
             ))}
+            </div>
         </AccordionPanel>
     );
 };
